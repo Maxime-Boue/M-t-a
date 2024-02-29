21f4a43029adb07f6809ab490964dd3ba1531d89
Maxime-Boue <maxime.boue@oclock.school>
2024-02-29 10:58:59 +0100

    change current weather to forecast weather, start with some bugs

--- src/components/ForecastWeather/ForecastWeather.tsx
+++ src/components/ForecastWeather/ForecastWeather.tsx
@@ -1,7 +1,10 @@
 function ForecastWeather() {
   return (
-    <div>
-      <h1>Coucou</h1>
+    <div className="flex justify-between w-[400px] py-4 pw-6 m-auto gap-1">
+      <div className="border w-[90px] h-[120px] bg-white rounded-lg" />
+      <div className="border w-[90px] h-[120px] bg-white rounded-lg" />
+      <div className="border w-[90px] h-[120px] bg-white rounded-lg" />
+      <div className="border w-[90px] h-[120px] bg-white rounded-lg" />
     </div>
   );
 }
--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -2,28 +2,31 @@ import { useEffect, useState } from 'react';
 import brume from '../../assets/brouillard.png';
 import couvert from '../../assets/couvert.png';
 import neige from '../../assets/neige.png';
-import peu_nuageux from '../../assets/partiellement_couvert.png';
+import peu_nuageux from '../../assets/peu_nuageux.png';
 import pluie from '../../assets/pluie.png';
 import soleil from '../../assets/soleil.png';
 
 type WeatherProps = {
   weatherData: {
-    main: {
-      temp: number;
-      feels_like: number;
-      humidity: number;
-      pressure: number;
-    };
-    wind: {
-      speed: number;
-    };
-    name: string;
-    sys: {
-      country: string;
-    };
-    weather: {
-      main: string;
-      description: string;
+    list: {
+      dt: number;
+      main: {
+        temp: number;
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+      };
+      wind: {
+        speed: number;
+      };
+      name: string;
+      sys: {
+        country: string;
+      };
+      weather: {
+        main: string;
+        description: string;
+      }[];
     }[];
   };
 };
@@ -32,9 +35,10 @@ function Weather({ weatherData }: WeatherProps) {
   const [icon, setIcon] = useState<string>('');
 
   useEffect(() => {
-    if (weatherData.weather && weatherData.weather.length > 0) {
-      switch (weatherData.weather[0].description.toLowerCase()) {
+    if (weatherData.list[0].weather && weatherData.list[0].weather.length > 0) {
+      switch (weatherData.list[0].weather[0].description.toLowerCase()) {
         case 'couvert':
+        case 'nuageux':
           setIcon(couvert);
           break;
         case 'brume':
@@ -43,9 +47,8 @@ function Weather({ weatherData }: WeatherProps) {
         case 'ciel dégagé':
           setIcon(soleil);
           break;
+        case 'bruine légère':
         case 'légère pluie':
-          setIcon(pluie);
-          break;
         case 'pluie modérée':
           setIcon(pluie);
           break;
@@ -59,23 +62,25 @@ function Weather({ weatherData }: WeatherProps) {
           setIcon('');
       }
     }
-  }, [weatherData.weather]);
+  }, [weatherData.list]);
 
   return (
     <div>
-      {weatherData.weather ? (
-        <article className="flex flex-col items-center justify-between w-[350px] h-[500px] bg-white shadow-lg rounded-lg m-auto py-3 px-6 top-[10%]">
+      {weatherData.list[0].weather ? (
+        <article className="flex flex-col items-center justify-between w-[400px] h-[500px] bg-white shadow-lg rounded-lg m-auto py-3 px-6 top-[10%]">
           <div className="flex flex-col items-center justify-between ">
             <p className="text-2xl py-4">
-              {weatherData.name}, {weatherData.sys.country}
+              {weatherData.list[0].name}, {weatherData.list[0].sys.country}
             </p>
             <div className="flex flex-col items-center gap-3">
               <img className="w-[150px] h-[150px]" src={icon} alt="" />
               <div className="flex flex-col justify-center items-center gap-2">
                 <p className="text-5xl font-semibold">
-                  {weatherData.main.temp.toFixed()} °C
+                  {weatherData.list[0].main.temp.toFixed()} °C
                 </p>
-                <p className="text-md">{weatherData.weather[0].description}</p>
+                <p className="text-md">
+                  {weatherData.list[0].weather[0].description}
+                </p>
               </div>
             </div>
           </div>
@@ -83,25 +88,25 @@ function Weather({ weatherData }: WeatherProps) {
             <div className="flex w-full">
               <p className="pl-8 w-1/2 ">Ressenti </p>
               <p className="text-center w-1/2 font-semibold">
-                {weatherData.main.feels_like.toFixed()} °C
+                {weatherData.list[0].main.feels_like.toFixed()} °C
               </p>
             </div>
             <div className="flex w-full">
               <p className="pl-8 w-1/2 ">Vent </p>
               <p className="text-center w-1/2 font-semibold">
-                {weatherData.wind.speed.toFixed()} km/h
+                {weatherData.list[0].wind.speed.toFixed()} km/h
               </p>
             </div>
             <div className="flex w-full">
               <p className="pl-8 w-1/2 ">Humidité </p>
               <p className="text-center w-1/2 font-semibold">
-                {weatherData.main.humidity.toFixed()} %
+                {weatherData.list[0].main.humidity.toFixed()} %
               </p>
             </div>
             <div className="flex w-full">
               <p className="pl-8 w-1/2 ">Pression </p>
               <p className="text-center w-1/2 font-semibold">
-                {weatherData.main.pressure.toFixed()} hPa
+                {weatherData.list[0].main.pressure.toFixed()} hPa
               </p>
             </div>
           </div>
